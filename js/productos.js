5cc417a8e66ac4f4dda55374565c30a0bd81f21e
Leovigildo <60432434@continental.edu.pe>
2026-05-31 22:26:52 -0500

    actualizado

--- js/productos.js
+++ js/productos.js
@@ -1,57 +1,57 @@
 const productos = [
     {
-        nombre: "Samsung Galaxy S25",
+        nombre: "Samsung Galaxy S24",
         precio: "S/ 3,499",
-        imagen: "",
+        imagen: "img/samsung-galaxy-s24.png",
         descripcion:
             "Pantalla Dynamic AMOLED 2X de 6.8 pulgadas, cámara principal de 200 MP y batería de larga duración."
     },
     {
         nombre: "iPhone 17",
         precio: "S/ 4,999",
-        imagen: "",
+        imagen: "img/iphone17.png",
         descripcion:
             "Chip de última generación, sistema avanzado de cámaras y pantalla Super Retina XDR."
     },
     {
-        nombre: "Xiaomi 16",
+        nombre: "Xiaomi 15t pro",
         precio: "S/ 2,299",
-        imagen: "",
+        imagen: "img/xiaomi 15t pro.jpg",
         descripcion:
             "Procesador de alto rendimiento, carga ultrarrápida y cámara optimizada con inteligencia artificial."
     },
     {
-        nombre: "Honor Magic 8",
+        nombre: "Honor X8",
         precio: "S/ 2,799",
-        imagen: "",
+        imagen: "img/honor x8.png",
         descripcion:
             "Diseño premium, pantalla OLED de alta resolución y excelente autonomía."
     },
     {
-        nombre: "OPPO Find X9",
+        nombre: "OPPO A38",
         precio: "S/ 3,199",
-        imagen: "",
+        imagen: "img/oppoA38.jpg",
         descripcion:
             "Sistema de cámaras profesionales, carga rápida SUPERVOOC y diseño elegante."
     },
     {
         nombre: "Google Pixel 10",
         precio: "S/ 3,899",
-        imagen: "",
+        imagen: "img/Google Pixel 10.jpg",
         descripcion:
             "Fotografía computacional avanzada, Android puro y actualizaciones garantizadas."
     },
     {
-        nombre: "Motorola Edge 60 Pro",
+        nombre: "Motorola G04",
         precio: "S/ 2,599",
-        imagen: "",
+        imagen: "img/motorola g04.png",
         descripcion:
             "Pantalla curva pOLED de 144 Hz, gran rendimiento y cámaras de alta calidad."
     },
     {
         nombre: "Nothing Phone (4)",
         precio: "S/ 2,999",
-        imagen: "",
+        imagen: "img/Nothing Phone (4).jpg",
         descripcion:
             "Diseño transparente distintivo, interfaz fluida y experiencia Android moderna."
     }
